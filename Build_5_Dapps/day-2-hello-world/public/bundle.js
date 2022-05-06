93ca56c9e38f624900c564884aeeb5887560aead
luxiaotong <luxiaotong713@sina.com>
2022-05-06 13:34:37 +0800

    Build 5 Dapps - day 2

---
+++ Build_5_Dapps/day-2-hello-world/public/bundle.js
@@ -0,0 +1,25 @@
+var contractABI = [
+    {
+        "inputs": [],
+        "name": "hello",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "pure",
+        "type": "function"
+    }
+];
+var contractAddress = '0x10f67E7622F632E61268364a5F0329AcAfFfAA38';
+var web3 = new Web3('http://localhost:9545');
+var helloWorld = new web3.eth.Contract(contractABI, contractAddress);
+
+document.addEventListener('DOM', () => {
+    helloWorld.methods.hello().call()
+        .then(result => {
+            document.getElementById('hello').innerHTML = result;
+        });
+});
--- README.md
+++ README.md
@@ -6,6 +6,27 @@ https://docs.soliditylang.org/en/v0.8.13/solidity-by-example.html
 
 ### Build 5 Dapps on the Ethereum Blockchain - Beginner Tutorial
 
+#### Doc
+
 https://www.youtube.com/watch?v=8wMKq7HvbKw&list=WL&index=1&t=70s
 
 https://github.com/jklepatch/eattheblocks/tree/master/dapp-30
+
+#### Command
+
+truffle init
+truffle test
+truffle compile
+truffle develop
+>> migrate --reset
+.exit
+
+#### Develop Order
+
+1. Code smart contract
+2. Start truffle console
+3. Deploy contract
+4. Copy contract ABI/address
+5. Instantiate web3
+6. Instantiate contract instance
+7. Call smart contract function
