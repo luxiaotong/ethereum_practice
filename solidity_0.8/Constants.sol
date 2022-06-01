f46a0780eb850ae6b6b9caeb219eee26581a7e20
Shannon Lu <luxiaotong713@sina.com>
2022-06-01 17:41:22 +0800

    solidity_0.8/Constants.sol

---
+++ solidity_0.8/Constants.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+contract Constants {
+    // 21442 gas
+   address public constant MY_ADDRESS = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+   uint public constant MY_UINT = 123;
+}
+
+contract Var {
+    // 23553 gas 
+   address public MY_ADDRESS = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+}
